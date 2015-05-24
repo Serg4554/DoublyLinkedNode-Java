01f0f8556901c84a10c07d5bc40deeb932701bcc
agprados <aidagp@uma.es>
2015-05-25 01:32:46 +0200

    Ampliado el test

--- src/test/DoublyLinkedListTest.java
+++ src/test/DoublyLinkedListTest.java
@@ -10,30 +10,39 @@ import org.junit.Test;
 public class DoublyLinkedListTest<T> {
 
   DoublyLinkedList<T> lista = null;
+  DoublyLinkedList<T> listaVacia = null;
+  DoublyLinkedList<Integer> listaNumeros = null;
 
   @Before
   public void inicializa() {
     lista = new DoublyLinkedList<T>(3);
+    listaVacia = new DoublyLinkedList<T>(0);
+    listaNumeros =  new DoublyLinkedList<Integer>(3);
   }
 
   @After
   public void finaliza() {
     lista = null;
+    listaVacia = null;
+    listaNumeros = null;
   }
 
   @Test
   public void testListaVacia() {
-    DoublyLinkedList<T> listaVacia = new DoublyLinkedList<T>(0);
+    int esperado = 0;
+    int real = listaVacia.getNodeList().size();
 
-    assertEquals(listaVacia.getNodeList().size(), 0);
+    assertEquals(esperado, real);
   }
 
   @Test
   public void testListaConUnNodo() {
     DoublyLinkedList<T> listaUnNodo = new DoublyLinkedList<T>(1);
+    int esperado = 1;
+    int real = listaUnNodo.getNodeList().size();
 
     assertNotNull(listaUnNodo);
-    assertEquals(listaUnNodo.getNodeList().size(), 1);
+    assertEquals(esperado, real);
   }
 
   @Test
@@ -44,8 +53,121 @@ public class DoublyLinkedListTest<T> {
   @Test
   public void testPosteriorDelUltimoNodoEsNull() {
     int tamano = lista.getNodeList().size();
+    Node<T> esperado = null;
+    Node<T> real = lista.getNodeList().get(tamano - 1).getNextNode();
 
-    assertEquals(lista.getNodeList().get(tamano - 1).getNextNode(), null);
+    assertEquals(esperado, real);
   }
+  
+  @Test
+  public void testInsertarNodoAlPrincipioListaVacia() {
+    Node<T> nodoNuevo = new Node<T>();
+    int esperado = 1;
+    
+    listaVacia.insertBeginning(listaVacia.getNodeList(), nodoNuevo);
+    int real = listaVacia.getNodeList().size();
+    
+    assertEquals(esperado, real);
+  }
+  
+  @Test
+  public void testInsertarNodoAlFinalListaVacia() {
+    Node<T> nodoNuevo = new Node<T>();
+    int esperado = 1;
+    
+    listaVacia.insertEnd(listaVacia.getNodeList(), nodoNuevo);
+    int real = listaVacia.getNodeList().size();
+    
+    assertEquals(esperado, real);
+  }
+  
+  @Test
+  public void testInsertarNodoAlPrincipioLista() {
+    Node<Integer> nodoNuevo = new Node<Integer>();
+    int esperado = listaNumeros.getNodeList().size() + 1;
+    int datoEsperado = 3;
+    
+    nodoNuevo.setNodeData(datoEsperado);
+    listaNumeros.insertBeginning(listaNumeros.getNodeList(), nodoNuevo);
+    int real = listaNumeros.getNodeList().size();
+    int datoReal = listaNumeros.getNodeList().get(0).getNodeData();
+    
+    assertEquals(datoEsperado, datoReal);
+    assertEquals(esperado, real);
+  }
+  
+  @Test
+  public void testInsertarNodoAlFinalLista() {
+    Node<Integer> nodoNuevo = new Node<Integer>();
+    int esperado = listaNumeros.getNodeList().size() + 1;
+    int datoEsperado = 3;
+    
+    nodoNuevo.setNodeData(datoEsperado);
+    listaNumeros.insertEnd(listaNumeros.getNodeList(), nodoNuevo);
+    int real = listaNumeros.getNodeList().size();
+    int datoReal = listaNumeros.getNodeList().get(real-1).getNodeData();
+    
+    assertEquals(datoEsperado, datoReal);
+    assertEquals(esperado, real);
+  }
+  
+  @Test
+  public void testInsertarNodoAlPrincipioDeOtroDado() {
+    Node<Integer> nodoNuevo = new Node<Integer>();
+    int esperado = listaNumeros.getNodeList().size() + 1;
+    int datoEsperado = 3;
+    
+    nodoNuevo.setNodeData(datoEsperado);
+    listaNumeros.insertBefore(listaNumeros.getNodeList(), listaNumeros.getNodeList().get(2), nodoNuevo);
+    int real = listaNumeros.getNodeList().size();
+    int datoReal = listaNumeros.getNodeList().get(2).getNodeData();
+    
+    assertEquals(datoEsperado, datoReal);
+    assertEquals(esperado, real);
+  }
+  
+  @Test
+  public void testInsertarNodoAlFinalDeOtroDado() {
+    Node<Integer> nodoNuevo = new Node<Integer>();
+    int esperado = listaNumeros.getNodeList().size() + 1;
+    int datoEsperado = 3;
+    
+    nodoNuevo.setNodeData(datoEsperado);
+    listaNumeros.insertAfter(listaNumeros.getNodeList(), listaNumeros.getNodeList().get(2), nodoNuevo);
+    int real = listaNumeros.getNodeList().size();
+    int datoReal = listaNumeros.getNodeList().get(3).getNodeData();
+    
+    assertEquals(datoEsperado, datoReal);
+    assertEquals(esperado, real);
+  }
+  
+  @Test
+  public void testStringListaVacia() {
+    String esperado = "";
+    String real = listaVacia.toString();
+    
+    assertEquals(esperado, real);
+  }
+  
+  @Test
+  public void testStringListaUnElemento() {
+    DoublyLinkedList<T> listaUnNodo = new DoublyLinkedList<T>(1);
+    String esperado = listaUnNodo.getNodeList().get(0).toString();
+    String real = listaUnNodo.toString();
+    
+    assertEquals(esperado, real);
+  }
+  
+ /* @Test
+  public void testStringListaTresEnteros() {
+    DoublyLinkedList<Integer> lista3Numeros =  new DoublyLinkedList<Integer>(0);
+    lista3Numeros.getNodeList().add(new Node<Integer>(3));
+    lista3Numeros.getNodeList().add(new Node<Integer>(4));
+    lista3Numeros.getNodeList().add(new Node<Integer>(1));
+    String esperado = "3->4->1";
+    String real = listaNumeros.toString();
+    
+    assertEquals(esperado, real);
+  }*/
 
 }
