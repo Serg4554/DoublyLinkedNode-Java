9badd6f79cf57ec6a8fac7d24737f4ef880b8995
agprados <aidagp@uma.es>
2015-05-24 23:40:22 +0200

    Añadido test

---
+++ src/test/DoublyLinkedListTest.java
@@ -0,0 +1,54 @@
+package org.iis.doublyLinkedNode;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DoublyLinkedListTest<T> {
+  
+  DoublyLinkedList<T> lista = null;
+ 
+  @Before
+  public void inicializa() {
+  lista = new DoublyLinkedList<T>(3);
+  }
+  
+  @After
+  public void finaliza() {
+  lista = null;
+  }
+  
+  @Test
+  public void testListaVacia() {
+    DoublyLinkedList<T> listaVacia = new DoublyLinkedList<T>(0);
+    
+    assertEquals(listaVacia.getNodeList().size(), 0);
+  }
+  
+  @Test
+  public void testListaConUnNodo() {
+    DoublyLinkedList<T> listaUnNodo = new DoublyLinkedList<T>(1);
+    
+    assertNotNull(listaUnNodo);
+    assertEquals(listaUnNodo.getNodeList().size(), 1);
+    assertEquals(listaUnNodo.getNodeList().get(0).getPrevNode(), null);
+    assertEquals(listaUnNodo.getNodeList().get(0).getNextNode(), null);
+  }
+  
+  @Test
+  public void testAnteriorDelPrimerNodoEsNull() {
+    assertEquals(lista.getNodeList().get(0).getPrevNode(), null);
+  }
+  
+  @Test
+  public void testPosteriorDelUltimoNodoEsNull() {
+    int tamano = lista.getNodeList().size();
+    
+    assertEquals(lista.getNodeList().get(tamano-1).getNextNode(), null);
+  }
+  
+  
+
+}
