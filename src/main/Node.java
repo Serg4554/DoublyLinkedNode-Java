2f702cd4f1b587c54ecd4efd435767df0c43102a
agprados <aidagp@uma.es>
2015-05-24 23:46:23 +0200

    Arreglo de errores menores

--- src/main/Node.java
+++ src/main/Node.java
@@ -1,39 +1,48 @@
+package org.iis.doublylinkednode;
 
 public class Node<T> {
-	private Node<T> prev;
-	private Node<T> next;
-	private T data;
-	
-	public Node() {
-		this.prev = null;
-		this.next = null;
-		this.data = null;
-	}
-	public void setPrevNode(Node<T> node) {
-		this.prev = node;
-	}
-	public void setNextNode(Node<T> node) {
-		this.next = node;
-	}
-	public void setNodeData(T data) {
-		this.data = data;
-	}
-	public Node<T> getPrevNode(Node<T> node) {
-		return this.prev;
-	}
-	public Node<T> getNextNode(Node<T> node) {
-		return this.next;
-	}
-	public T getNodeData(T data) {
-		return this.data;
-	}
-	public String toString() {
-		String str="";
-		try {
-			str = data.toString();
-		} catch(NullPointerException e) {
-			str="Empty";
-		}
-		return str;
-	}
+  private Node<T> prev;
+  private Node<T> next;
+  private T data;
+
+  public Node() {
+    this.prev = null;
+    this.next = null;
+    this.data = null;
+  }
+
+  public void setPrevNode(Node<T> node) {
+    this.prev = node;
+  }
+
+  public void setNextNode(Node<T> node) {
+    this.next = node;
+  }
+
+  public void setNodeData(T data) {
+    this.data = data;
+  }
+
+  public Node<T> getPrevNode() {
+    return this.prev;
+  }
+
+  public Node<T> getNextNode() {
+    return this.next;
+  }
+
+  public T getNodeData() {
+    return this.data;
+  }
+
+  @Override
+  public String toString() {
+    String str = "";
+    try {
+      str = data.toString();
+    } catch (NullPointerException e) {
+      str = "Empty";
+    }
+    return str;
+  }
 }
--- src/test/DoublyLinkedListTest.java
+++ src/test/DoublyLinkedListTest.java
@@ -1,54 +1,51 @@
-package org.iis.doublyLinkedNode;
+package org.iis.doublylinkednode;
 
 import static org.junit.Assert.*;
 
+import org.iis.doublylinkednode.DoublyLinkedList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class DoublyLinkedListTest<T> {
-  
+
   DoublyLinkedList<T> lista = null;
- 
+
   @Before
   public void inicializa() {
-  lista = new DoublyLinkedList<T>(3);
+    lista = new DoublyLinkedList<T>(3);
   }
-  
+
   @After
   public void finaliza() {
-  lista = null;
+    lista = null;
   }
-  
+
   @Test
   public void testListaVacia() {
     DoublyLinkedList<T> listaVacia = new DoublyLinkedList<T>(0);
-    
+
     assertEquals(listaVacia.getNodeList().size(), 0);
   }
-  
+
   @Test
   public void testListaConUnNodo() {
     DoublyLinkedList<T> listaUnNodo = new DoublyLinkedList<T>(1);
-    
+
     assertNotNull(listaUnNodo);
     assertEquals(listaUnNodo.getNodeList().size(), 1);
-    assertEquals(listaUnNodo.getNodeList().get(0).getPrevNode(), null);
-    assertEquals(listaUnNodo.getNodeList().get(0).getNextNode(), null);
   }
-  
+
   @Test
   public void testAnteriorDelPrimerNodoEsNull() {
     assertEquals(lista.getNodeList().get(0).getPrevNode(), null);
   }
-  
+
   @Test
   public void testPosteriorDelUltimoNodoEsNull() {
     int tamano = lista.getNodeList().size();
-    
-    assertEquals(lista.getNodeList().get(tamano-1).getNextNode(), null);
+
+    assertEquals(lista.getNodeList().get(tamano - 1).getNextNode(), null);
   }
-  
-  
 
 }
